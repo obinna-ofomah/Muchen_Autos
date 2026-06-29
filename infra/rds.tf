5d5f37ae7b0d3f45d92431b2b86f59e78019628f
obinna-ofomah <jano.obinna@gmail.com>
2026-06-29 12:18:10 +0100

    modified the infra config file

--- infra/rds.tf
+++ infra/rds.tf
@@ -1,10 +1,10 @@
 resource "aws_db_instance" "lambda_rds" {
   allocated_storage   = 10
-  db_name             = "obinnadb"
+  db_name             = var.dbname
   engine              = "postgres"
   instance_class      = "db.t3.micro"
-  username            = "obinna"
-  password            = "password"
+  username            = var.username
+  password            = var.password
   skip_final_snapshot = true
   port                = 5432
   publicly_accessible = false
